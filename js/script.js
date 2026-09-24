f513bbb777b75c6eec553c8bc1a91ce408854d0d
gabriellineumann <neumann.gabrielli@escola.pr.gov.br>
2026-09-24 11:46:42 -0300

    script

--- js/script.js
+++ js/script.js
@@ -35,10 +35,7 @@ function respostaSelecionada(opcaoSelecionada) {
     mostraPergunta();
 }
  
-function aleatorio(lista) {
-const posicao = Math.floor(Math.random()* lista.length);
-return lista[posicao];
-} 
+
 
 function mostraResultado(){
     caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
